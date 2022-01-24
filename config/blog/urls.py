91f8b6fc8338047bdcfce55c4d73190c99320302
Daniyal Malik <myEmail@example.com>
2022-01-24 20:27:42 +0500

    Complete the Chapter 5 Successfully

--- config/blog/urls.py
+++ config/blog/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import BlogListView
+from .views import BlogListView,BlogDetailView
 urlpatterns = [
-    path("", BlogListView.as_view(), name="home")
+    path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
+    path("", BlogListView.as_view(), name="home"),
 ]
