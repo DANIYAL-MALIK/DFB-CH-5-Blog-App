4d6d60aff9354062fc8398fa76121450100ffd92
Daniyal Malik <myEmail@example.com>
2022-01-25 18:58:47 +0500

    Add the reverse function and the form is working correctly

--- config/blog/urls.py
+++ config/blog/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import BlogListView,BlogDetailView
+from .views import BlogListView,BlogDetailView,BlogCreateView
 urlpatterns = [
+    path("post/new",BlogCreateView.as_view(), name="post_new"),
     path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
     path("", BlogListView.as_view(), name="home"),
 ]
--- config/blog/views.py
+++ config/blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import  ListView,DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
 class BlogListView(ListView):
     model=Post
     template_name="home.html"
-# Create your views here.
 class BlogDetailView(DetailView):
     model=Post
     template_name="postdetail.html"
+class BlogCreateView(CreateView):
+    model=Post
+    template_name="post_new.html"
+    fields=['title','author','body']
